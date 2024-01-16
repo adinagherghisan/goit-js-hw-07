4f61a0380fe69bc6b3a0faf4bde7eee223aaa62a
adinagherghisan <gherghisanadina@gmail.com>
2024-01-16 16:51:34 +0200

    Create gallery items.js

---
+++ js/gallery-items.js
@@ -0,0 +1,65 @@
+export const galleryItems = [
+  {
+    preview:
+      '/img/Hokkaido Flower-preview.jpg',
+    original:
+      '/img/Hokkaido Flower-original.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      '/img/Container Haulage Freight-preview.jpg',
+    original:
+      '/img/Container Haulage Freight-original.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      '/img/Aerial Beach View-preview.jpg',
+    original:
+      '/img/Aerial Beach View-original.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      '/img/Flower Blooms-preview.jpg',
+    original:
+      '/img/Flower Blooms-original.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      '/img/Alpine Mountains-preview.jpg',
+    original:
+      '/img/Alpine Mountains-original.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      '/img/Mountain Lake Sailing-preview.jpg',
+    original:
+      '/img/Mountain Lake Sailing-original.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      '/img/Alpine Spring Meadows-preview.jpg',
+    original:
+      '/img/Alpine Spring Meadows-original.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      '/img/Nature Landscape-preview.jpg',
+    original:
+      '/img/Nature Landscape-original.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      '/img/Lighthouse Coast Sea-preview.jpg',
+    original:
+      '/img/Lighthouse Coast Sea-original.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
