e5f6352eb88ead68bca838dfceecb1e2b133ae42
adinagherghisan <gherghisanadina@gmail.com>
2024-01-18 11:25:07 +0200

    update path img for gallery items

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,65 +1,65 @@
 export const galleryItems = [
   {
     preview:
-      '/img/Hokkaido Flower-preview.jpg',
+      './img/Hokkaido Flower-preview.jpg',
     original:
-      '/img/Hokkaido Flower-original.jpg',
+      './img/Hokkaido Flower-original.jpg', 
     description: 'Hokkaido Flower',
   },
   {
     preview:
-      '../img/Container Haulage Freight-preview.jpg',
+      './img/Container Haulage Freight-preview.jpg',
     original:
-      '../img/Container Haulage Freight-original.jpg',
+      './img/Container Haulage Freight-original.jpg',
     description: 'Container Haulage Freight',
   },
   {
     preview:
-      '../img/Aerial Beach View-preview.jpg',
+      './ img / Aerial Beach View- preview.jpg',
     original:
-      '../img/Aerial Beach View-original.jpg',
+      './img/Aerial Beach View-original.jpg',
     description: 'Aerial Beach View',
   },
   {
     preview:
-      '../img/Flower Blooms-preview.jpg',
+      './img/Flower Blooms-preview.jpg',
     original:
-      '../img/Flower Blooms-original.jpg',
+      './img/Flower Blooms-original.jpg',
     description: 'Flower Blooms',
   },
   {
     preview:
-      '../img/Alpine Mountains-preview.jpg',
+      './img/Alpine Mountains-preview.jpg',
     original:
-      '../img/Alpine Mountains-original.jpg',
+      './img/Alpine Mountains-original.jpg',
     description: 'Alpine Mountains',
   },
   {
     preview:
-      '../img/Mountain Lake Sailing-preview.jpg',
+      './img/Mountain Lake Sailing-preview.jpg',
     original:
-      '../img/Mountain Lake Sailing-original.jpg',
+      './img/Mountain Lake Sailing-original.jpg',
     description: 'Mountain Lake Sailing',
   },
   {
     preview:
-      '../img/Alpine Spring Meadows-preview.jpg',
+      './img/Alpine Spring Meadows-preview.jpg',
     original:
-      '../img/Alpine Spring Meadows-original.jpg',
+      './img/Alpine Spring Meadows-original.jpg',
     description: 'Alpine Spring Meadows',
   },
   {
     preview:
-      '../img/Nature Landscape-preview.jpg',
+      './img/Nature Landscape-preview.jpg',
     original:
-      '../img/Nature Landscape-original.jpg',
+      './img/Nature Landscape-original.jpg',
     description: 'Nature Landscape',
   },
   {
     preview:
-      '../img/Lighthouse Coast Sea-preview.jpg',
+      './img/Lighthouse Coast Sea-preview.jpg',
     original:
-      '../img/Lighthouse Coast Sea-original.jpg',
+      './img/Lighthouse Coast Sea-original.jpg',
     description: 'Lighthouse Coast Sea',
   },
 ];
